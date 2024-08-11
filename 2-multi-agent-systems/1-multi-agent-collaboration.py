b4c6cbb7050b662a87f0e4c5e558238273d049d9
amir-rafieian <rafieian.amir@gmail.com>
2024-08-11 19:25:46 +0800

    added invoke sample

--- 2-multi-agent-systems/1-multi-agent-collaboration.py
+++ 2-multi-agent-systems/1-multi-agent-collaboration.py
@@ -240,6 +240,22 @@ graph.get_graph().print_ascii()
 # =============================================================================
 # Invoke
 # =============================================================================
+events = graph.stream(
+    {
+        "messages": [
+            HumanMessage(
+                content="Fetch the UK's GDP over the past 5 years,"
+                " then draw a line graph of it."
+                " Once you code it up, finish."
+            )
+        ],
+    },
+    # Maximum number of steps to take in the graph
+    {"recursion_limit": 150},
+)
+for s in events:
+    print(s)
+    print("----")
 
 
 
