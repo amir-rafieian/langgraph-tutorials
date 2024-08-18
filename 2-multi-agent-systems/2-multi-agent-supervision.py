d6afa5f9ab97e6d64d88ff24c433c6fb976bc0ea
amir-rafieian <rafieian.amir@gmail.com>
2024-08-18 20:07:00 +0800

    defined tools

--- 2-multi-agent-systems/2-multi-agent-supervision.py
+++ 2-multi-agent-systems/2-multi-agent-supervision.py
@@ -1,7 +1,49 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 11 20:11:17 2024
+# =============================================================================
+# Agent Supervisor
+# The collaboration example routed messages automatically based on the output of the initial 
+# researcher agent.
+# 
+# We can also choose to use an LLM to orchestrate the different agents.
+# Below, we will create an agent group, with an agent supervisor to help delegate tasks.
+# =============================================================================
 
-@author: rafie
-"""
+# =============================================================================
+# Import Libraries
+# =============================================================================
+import getpass
+import os
+from dotenv import load_dotenv
+from IPython.display import Image, display
+from typing import Annotated, Sequence, TypedDict, Literal
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_experimental.tools import PythonREPLTool
+
+# =============================================================================
+# Load env variables
+# =============================================================================
+load_dotenv()
+
+def _set_if_undefined(var:str):
+    if var not in os.environ.keys():
+        os.environ[var] = getpass(f"Please provide your {var}")
+
+_set_if_undefined("OPENAI_API_KEY")
+_set_if_undefined("LANGCHAIN_API_KEY")
+_set_if_undefined("TAVILY_API_KEY")
+
+
+# =============================================================================
+# Create Tools
+# =============================================================================
+# Tool1: Tavily Search
+tavily_tool = TavilySearchResults(max_results=5)
+
+# Tools2: Python Executer. This executes code locally, which can be unsafe
+python_repl_tool = PythonREPLTool()
+
+
+# =============================================================================
+# Helper Utilities
+# =============================================================================
 
