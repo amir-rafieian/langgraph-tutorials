64df97b64f8b905466eb478f7cd4e77e15a22a6f
amir-rafieian <rafieian.amir@gmail.com>
2024-09-01 17:41:08 +0800

    defined doc writing team graph

--- 2-multi-agent-systems/3-multi-agent-hierarchical-supervision.py
+++ 2-multi-agent-systems/3-multi-agent-hierarchical-supervision.py
@@ -37,6 +37,8 @@ from tempfile import TemporaryDirectory
 import functools
 import operator
 
+from langgraph.prebuilt import create_react_agent
+
 _TEMP_DIRECTORY = TemporaryDirectory()
 WORKING_DIRECTORY = Path(_TEMP_DIRECTORY.name)
 
@@ -174,36 +176,35 @@ def python_repl(
 # Create a worker agent.
 # Create a supervisor for the sub-graph.
 
-def create_agent(
-    llm: ChatOpenAI,
-    tools: list,
-    system_prompt: str,
-) -> str:
-    """Create a function-calling agent and add it to the graph."""
-    system_prompt += "\nWork autonomously according to your specialty, using the tools available to you."
-    " Do not ask for clarification."
-    " Your other team members (and other teams) will collaborate with you with their own specialties."
-    " You are chosen for a reason! You are one of the following team members: {team_members}."
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                system_prompt,
-            ),
-            MessagesPlaceholder(variable_name="messages"),
-            MessagesPlaceholder(variable_name="agent_scratchpad"),
-        ]
-    )
-    agent = create_openai_functions_agent(llm, tools, prompt)
-    executor = AgentExecutor(agent=agent, tools=tools)
-    return executor
-
-
-
 def agent_node(state, agent, name):
     result = agent.invoke(state)
-    return {"messages": [HumanMessage(content=result["output"], name=name)]}
+    return {"messages": [HumanMessage(content=result["messages"][-1].content, name=name)]}
 
+
+
+# def create_agent(
+#     llm: ChatOpenAI,
+#     tools: list,
+#     system_prompt: str,
+# ) -> str:
+#     """Create a function-calling agent and add it to the graph."""
+#     system_prompt += "\nWork autonomously according to your specialty, using the tools available to you."
+#     " Do not ask for clarification."
+#     " Your other team members (and other teams) will collaborate with you with their own specialties."
+#     " You are chosen for a reason! You are one of the following team members: {team_members}."
+#     prompt = ChatPromptTemplate.from_messages(
+#         [
+#             (
+#                 "system",
+#                 system_prompt,
+#             ),
+#             MessagesPlaceholder(variable_name="messages"),
+#             MessagesPlaceholder(variable_name="agent_scratchpad"),
+#         ]
+#     )
+#     agent = create_openai_functions_agent(llm, tools, prompt)
+#     executor = AgentExecutor(agent=agent, tools=tools)
+#     return executor
 
 
 def create_team_supervisor(llm: ChatOpenAI, system_prompt, members) -> str:
@@ -264,23 +265,10 @@ class ResearchTeamState(TypedDict):
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 
-search_agent = create_agent(
-    llm,
-    [tavily_tool],
-    "You are a research assistant who can search for up-to-date info using the tavily search engine.",
-)
-
+search_agent = create_react_agent(llm, tools=[tavily_tool])
 search_node = functools.partial(agent_node, agent=search_agent, name="Search")
 
-
-
-
-research_agent = create_agent(
-    llm,
-    [scrape_webpages],
-    "You are a research assistant who can scrape specified urls for more detailed information using the scrape_webpages function.",
-)
-
+research_agent = create_react_agent(llm, tools=[scrape_webpages])
 research_node = functools.partial(agent_node, agent=research_agent, name="WebScraper")
 
 
@@ -348,6 +336,148 @@ for s in research_chain.stream(
 # # Define Agent Teams - Document Writing Team
 # =============================================================================
 # =============================================================================
+# Note that we are giving file-system access to our agent here, which is not safe in all cases.
+
+# Document writing team graph state
+class DocWritingState(TypedDict):
+    # This tracks the team's conversation internally
+    messages: Annotated[List[BaseMessage], operator.add]
+    # This provides each worker with context on the others' skill sets
+    team_members: str
+    # This is how the supervisor tells langgraph who to work next
+    next: str
+    # This tracks the shared directory state
+    current_files: str
+
+
+# This will be run before each worker agent begins work
+# It makes it so they are more aware of the CURRENT state
+# of the working directory before each call.
+
+def prelude(state):
+    written_files = []
+    if not WORKING_DIRECTORY.exists():
+        WORKING_DIRECTORY.mkdir()
+    try:
+        written_files = [
+            f.relative_to(WORKING_DIRECTORY) for f in WORKING_DIRECTORY.rglob("*")
+        ]
+    except Exception:
+        pass
+    if not written_files:
+        return {**state, "current_files": "No files written."}
+    return {
+        **state,
+        "current_files": "\nBelow are files your team has written to the directory:\n"
+        + "\n".join([f" - {f}" for f in written_files]),
+    }
+
+
+llm = ChatOpenAI(model="gpt-4o")
+
+
+# Doc writer agent & node:
+doc_writer_agent = create_react_agent(llm, tools=[write_document, edit_document, read_document])
+# Injects current directory working state before each call
+context_aware_doc_writer_agent = prelude | doc_writer_agent
+
+doc_writing_node = functools.partial(
+    agent_node, agent=context_aware_doc_writer_agent, name="DocWriter"
+)
+
+
+# Note Taking Agent & node:
+note_taking_agent = create_react_agent(llm,tools=[create_outline, read_document])
+context_aware_note_taking_agent = prelude | note_taking_agent
+note_taking_node = functools.partial(
+    agent_node, agent=context_aware_note_taking_agent, name="NoteTaker"
+)
+
+
+# Chart generating Agent & node:
+chart_generating_agent = create_react_agent(llm, tools=[read_document, python_repl])
+context_aware_chart_generating_agent = prelude | chart_generating_agent
+chart_generating_node = functools.partial(
+    agent_node, agent=context_aware_chart_generating_agent, name="ChartGenerator"
+)
+
+
+doc_writing_supervisor = create_team_supervisor(
+    llm,
+    "You are a supervisor tasked with managing a conversation between the"
+    " following workers:  {team_members}. Given the following user request,"
+    " respond with the worker to act next. Each worker will perform a"
+    " task and respond with their results and status. When finished,"
+    " respond with FINISH.",
+    ["DocWriter", "NoteTaker", "ChartGenerator"],
+)
+
+
+# =============================================================================
+# Create the Docwriting Team graph
+# =============================================================================
+authoring_graph = StateGraph(DocWritingState)
+authoring_graph.add_node("DocWriter", doc_writing_node)
+authoring_graph.add_node("NoteTaker", note_taking_node)
+authoring_graph.add_node("ChartGenerator", chart_generating_node)
+authoring_graph.add_node("supervisor", doc_writing_supervisor)
+
+# Add the edges that always occur
+authoring_graph.add_edge("DocWriter", "supervisor")
+authoring_graph.add_edge("NoteTaker", "supervisor")
+authoring_graph.add_edge("ChartGenerator", "supervisor")
+
+
+# Add the edges where routing applies
+authoring_graph.add_conditional_edges(
+    "supervisor",
+    lambda x: x["next"],
+    {
+        "DocWriter": "DocWriter",
+        "NoteTaker": "NoteTaker",
+        "ChartGenerator": "ChartGenerator",
+        "FINISH": END,
+    },
+)
+
+authoring_graph.add_edge(START, "supervisor")
+chain = authoring_graph.compile()
+
+
+# The following functions interoperate between the top level graph state
+# and the state of the research sub-graph
+# this makes it so that the states of each graph don't get intermixed
+def enter_chain(message: str, members: List[str]):
+    results = {
+        "messages": [HumanMessage(content=message)],
+        "team_members": ", ".join(members),
+    }
+    return results
+
+
+# We reuse the enter/exit functions to wrap the graph
+authoring_chain = (
+    functools.partial(enter_chain, members=authoring_graph.nodes)
+    | authoring_graph.compile()
+)
+
+
+# Visualize the graph
+Image(chain.get_graph(xray=True).draw_mermaid_png())
+
+
+for s in authoring_chain.stream(
+    "Write an outline for poem and then write the poem to disk.",
+    {"recursion_limit": 100},
+):
+    if "__end__" not in s:
+        print(s)
+        print("---")
+
+
+
+
+
 
 
 
