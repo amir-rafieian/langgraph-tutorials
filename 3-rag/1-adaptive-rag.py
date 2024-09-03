af6a3ecf02777d1cebb417b41cc594105e33bb72
amir-rafieian <rafieian.amir@gmail.com>
2024-09-03 19:50:34 +0800

    added Retrieval Grader

--- 3-rag/1-adaptive-rag.py
+++ 3-rag/1-adaptive-rag.py
@@ -78,6 +78,10 @@ retriever = vectorstore.as_retriever()
 # # LLMS
 # =============================================================================
 # =============================================================================
+
+# =============================================================================
+# Router LLM
+# =============================================================================
 # Data model
 class RouteQuery(BaseModel):
     """Route a user query to the most relevant datasource."""
@@ -109,14 +113,46 @@ question_router.invoke({"question": "Who will the Bears draft first in the NFL d
 
 question_router.invoke({"question": "What are the types of agent memory?"})  # RouteQuery(datasource='vectorstore')
 
+# =============================================================================
+# Retrieval Grader LLM
+# =============================================================================
+# Data model
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents."""
+
+    binary_score: str = Field(
+        description="Documents are relevant to the question, 'yes' or 'no'"
+    )
+
+# LLM with function call
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+structured_llm_grader = llm.with_structured_output(GradeDocuments)
+
+# Prompt
+system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
+    If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+    It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
 
 
+grade_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
+    ]
+)
 
 
+retrieval_grader = grade_prompt | structured_llm_grader
+question = "agent memory"
+docs = retriever.get_relevant_documents(question)
+doc_txt = docs[1].page_content
+print(retrieval_grader.invoke({"question": question, "document": doc_txt}))  # binary_score='no'
 
 
-
-
+# =============================================================================
+# Generate LLM
+# =============================================================================
 
 
 
