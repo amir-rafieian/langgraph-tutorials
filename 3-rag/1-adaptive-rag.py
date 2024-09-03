49b9e26a00d2f58a63f347926094c5f57c551da4
amir-rafieian <rafieian.amir@gmail.com>
2024-09-03 20:03:21 +0800

    added answer grader llm

--- 3-rag/1-adaptive-rag.py
+++ 3-rag/1-adaptive-rag.py
@@ -205,9 +205,39 @@ hallucination_prompt = ChatPromptTemplate.from_messages(
 hallucination_grader = hallucination_prompt | structured_llm_hallucination_grader
 hallucination_grader.invoke({"documents": docs, "generation": generation})  # GradeHallucinations(binary_score='yes')
 
+# =============================================================================
+# Answer Grader LLM
+# =============================================================================
+# Data model
+class GradeAnswer(BaseModel):
+    """Binary score to assess answer addresses question."""
+
+    binary_score: str = Field(
+        description="Answer addresses the question, 'yes' or 'no'"
+    )
 
 
+# LLM with function call
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+structured_llm_answer_grader = llm.with_structured_output(GradeAnswer)
 
+# Prompt
+system = """You are a grader assessing whether an answer addresses / resolves a question \n 
+     Give a binary score 'yes' or 'no'. Yes' means that the answer resolves the question."""
+answer_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
+    ]
+)
+
+answer_grader = answer_prompt | structured_llm_answer_grader
+answer_grader.invoke({"question": question, "generation": generation})  # GradeAnswer(binary_score='yes')
+
+
+# =============================================================================
+# Question Re-writer LLM
+# =============================================================================
 
 
 
