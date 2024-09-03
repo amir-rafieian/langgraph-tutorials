b4161260770adf76b3a04e241c6908862563bfaa
amir-rafieian <rafieian.amir@gmail.com>
2024-09-03 19:45:05 +0800

    added router llm to select websearch or vectordb

--- 3-rag/1-adaptive-rag.py
+++ 3-rag/1-adaptive-rag.py
@@ -20,6 +20,9 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_openai import ChatOpenAI
 
 # =============================================================================
 # Load env variables
@@ -60,7 +63,7 @@ text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
 
 doc_splits = text_splitter.split_documents(docs_list)
 
-# Add to vectorstore
+# Add to vectorstore (Here you may get write access error, you can switch to proper vector databases such as pinecone)
 vectorstore = Chroma.from_documents(
     documents=doc_splits,
     collection_name="rag-chroma",
@@ -70,11 +73,41 @@ vectorstore = Chroma.from_documents(
 # Create Retriever
 retriever = vectorstore.as_retriever()
 
+# =============================================================================
+# =============================================================================
+# # LLMS
+# =============================================================================
+# =============================================================================
+# Data model
+class RouteQuery(BaseModel):
+    """Route a user query to the most relevant datasource."""
 
+    datasource: Literal["vectorstore", "web_search"] = Field(...,
+        description="Given a user question choose to route it to web search or a vectorstore.",
+    )
+    
 
+# LLM with function call
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+structured_llm_router = llm.with_structured_output(RouteQuery)
 
+# Prompt
+system = """You are an expert at routing a user question to a vectorstore or web search.
+The vectorstore contains documents related to agents, prompt engineering, and adversarial attacks.
+Use the vectorstore for questions on these topics. Otherwise, use web-search."""
 
+route_prompt = ChatPromptTemplate.from_messages(
+    [
+         ("system", system),
+         ("human", "{question}")
+     ]
+    )
 
+question_router = route_prompt | structured_llm_router
+
+question_router.invoke({"question": "Who will the Bears draft first in the NFL draft?"})  # RouteQuery(datasource='vectorstore')
+
+question_router.invoke({"question": "What are the types of agent memory?"})  # RouteQuery(datasource='vectorstore')
 
 
 
