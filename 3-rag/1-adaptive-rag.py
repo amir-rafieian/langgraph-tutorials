4b0d965bead9e292ca6b7c4af987c933c6b49ab9
amir-rafieian <rafieian.amir@gmail.com>
2024-09-03 22:45:49 +0800

    added the graph

--- 3-rag/1-adaptive-rag.py
+++ 3-rag/1-adaptive-rag.py
@@ -31,7 +31,10 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 
 from langchain.schema import Document
 
-import pprint
+from pprint import pprint
+
+
+from langgraph.graph import END, StateGraph, START
 
 # =============================================================================
 # Load env variables
@@ -408,10 +411,6 @@ def web_search(state):
     return {"documents": web_results, "question": question}
 
 
-# =============================================================================
-# Edges
-# =============================================================================
-
 def route_question(state):
     """
     Route question to web search or RAG.
@@ -506,12 +505,83 @@ def grade_generation_v_documents_and_question(state):
 # =============================================================================
 # Build Graph
 # =============================================================================
+workflow = StateGraph(GraphState)
+
+# Define the nodes
+workflow.add_node("web_search", web_search)  # web search
+workflow.add_node("retrieve", retrieve)  # retrieve
+workflow.add_node("grade_documents", grade_documents)  # grade documents
+workflow.add_node("generate", generate)  # generatae
+workflow.add_node("transform_query", transform_query)  # transform_query
 
 
+workflow.add_conditional_edges(
+    START,
+    route_question,
+    {
+        "web_search": "web_search",
+        "vectorstore": "retrieve",
+    },
+)
+
+workflow.add_edge("web_search", "generate")
+workflow.add_edge("retrieve", "grade_documents")
+
+workflow.add_conditional_edges(
+    "grade_documents",
+    decide_to_generate,
+    {
+        "transform_query": "transform_query",
+        "generate": "generate",
+    },
+)
 
 
+workflow.add_edge("transform_query", "retrieve")
 
 
+workflow.add_conditional_edges(
+    "generate",
+    grade_generation_v_documents_and_question,
+    {
+        "not supported": "generate",
+        "useful": END,
+        "not useful": "transform_query",
+    },
+)
+
+# Compile
+app = workflow.compile()
+
+
+# Run
+inputs = {
+    "question": "What player at the Bears expected to draft first in the 2024 NFL draft?"
+}
+for output in app.stream(inputs):
+    for key, value in output.items():
+        # Node
+        pprint(f"Node '{key}':")
+        # Optional: print full state at each node
+        # pprint.pprint(value["keys"], indent=2, width=80, depth=None)
+    pprint("\n---\n")
+
+# Final generation
+pprint(value["generation"])
+
+
+# Run
+inputs = {"question": "What are the types of agent memory?"}
+for output in app.stream(inputs):
+    for key, value in output.items():
+        # Node
+        pprint(f"Node '{key}':")
+        # Optional: print full state at each node
+        # pprint.pprint(value["keys"], indent=2, width=80, depth=None)
+    pprint("\n---\n")
+
+# Final generation
+pprint(value["generation"])
 
 
 
