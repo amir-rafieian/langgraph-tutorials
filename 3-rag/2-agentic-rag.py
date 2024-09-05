b30a4d523f4b9cba2ceba362042dc70097ac1383
amir-rafieian <rafieian.amir@gmail.com>
2024-09-05 18:07:55 +0800

    added rag retriever tool

--- 3-rag/2-agentic-rag.py
+++ 3-rag/2-agentic-rag.py
@@ -1,7 +1,182 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  3 22:58:29 2024
+# =============================================================================
+# Retrieval Agents are useful when we want to make decisions about whether to retrieve 
+# from an index.
+# 
+# To implement a retrieval agent, we simple need to give an LLM access to a retriever tool.
+# =============================================================================
 
-@author: rafie
-"""
+# =============================================================================
+# Import Libraries
+# =============================================================================
+import getpass
+import os
+from dotenv import load_dotenv
+from IPython.display import Image, display
+from typing import Annotated, Sequence, TypedDict, Literal, List
+
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain.tools.retriever import create_retriever_tool
+# =============================================================================
+# Load env variables
+# =============================================================================
+load_dotenv()
+
+def _set_if_undefined(var:str):
+    if var not in os.environ.keys():
+        os.environ[var] = getpass(f"Please provide your {var}")
+
+_set_if_undefined("OPENAI_API_KEY")
+_set_if_undefined("LANGCHAIN_API_KEY")
+
+
+# =============================================================================
+# Create Embedding & Chrome index & Retreiver (Here you may get write access error, you can switch to proper vector databases such as pinecone)
+# =============================================================================
+embd = OpenAIEmbeddings()
+
+
+# Get some docs and add them to chroma db
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+
+# Splitter
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=500, chunk_overlap=0
+)
+
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorstore (Here you may get write access error, you can switch to proper vector databases such as pinecone)
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=embd,
+)
+
+# Create Retriever
+retriever = vectorstore.as_retriever()
+
+
+# =============================================================================
+# Create Retriever Tool
+# =============================================================================
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts on LLM agents, prompt engineering, and adversarial attacks on LLMs.",
+)
+
+tools = [retriever_tool]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
